58f15d3ed2e64bbe7d49ea648d77b7253f702929
Justin Chau <chausicle@gmail.com>
2018-01-23 15:53:55 -0800

    Made partial psudocode

---
+++ crypto-square.js
@@ -0,0 +1,12 @@
+/*
+input(s): A string of letters, punctuations, spaces, and numbers
+output(s): A string of letters strung together
+
+Psudocode:
+take a input string
+remove all punctuations with and whitespaces with Regex
+check if string is even or odd
+  if even --> row and column # are same
+  if odd --> column is rounded up and row is rounded down of the half of length of string
+create an array that will store each element a string that its length is LESS THAN OR EQUAL TO column
+*/
