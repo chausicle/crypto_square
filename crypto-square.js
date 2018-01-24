23df53996e9d5f3b37105f9a932bcbadb9f24a71
Justin Chau <chausicle@gmail.com>
2018-01-23 16:26:25 -0800

    Fixed invalid row and col values

--- crypto-square.js
+++ crypto-square.js
@@ -13,12 +13,27 @@ create an array that will store each element a string that its length is LESS TH
 
 function crypto (message) {
   let str = message.replace(/[^\w\s]|_ /g, "").split(' ').join('');
+  let row = 0;
+  let col = 0;
+
+  if (str.length % 2 === 0) {
+    col = str.length / 2;
+    row = str.length / 2;
+  } else {
+    col = Math.ceil(str.length / 2);
+    row = Math.floor(str.length / 2);
+  }
+
+  console.log(`${str}
+    row=${row} , col=${col}`);
 
   return str;
 }
 
 // Test cases
 if(crypto('ZOMG! ZOMBIES!!!This1234!!./#$%^&&^%2134#$%f#f45`') === 'ZOMGZOMBIESThis12342134ff45') console.log('PASSING WITH FLYING COLORS');
-else console.log('FAILING MISERABLY, TRY AGAIN, YOU GOT DIS!')
+else console.log('FAILING MISERABLY, TRY AGAIN, YOU GOT DIS!');
+if(crypto('Time 2is 3an 4illusion. 5Lunchtime 6doubly so.') === 'Time2is3an4illusion5Lunchtime6doublyso') console.log('PASSING WITH FLYING COLORS');
+else console.log('FAILING MISERABLY, TRY AGAIN, YOU GOT DIS!');
 
-console.log(crypto('ZOMG! ZOMBIES!!!This1234!!./#$%^&&^%2134#$%f#f45`'));
+// console.log(crypto('ZOMG! ZOMBIES!!!This1234!!./#$%^&&^%2134#$%f#f45`'));
