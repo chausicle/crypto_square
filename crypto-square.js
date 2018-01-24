54d4834d4936d5ebaebf318739e9ac8eca7ffa70
Justin Chau <chausicle@gmail.com>
2018-01-23 16:15:53 -0800

    Added test cases and removed string of anything not a letter or number

--- crypto-square.js
+++ crypto-square.js
@@ -10,3 +10,15 @@ check if string is even or odd
   if odd --> column is rounded up and row is rounded down of the half of length of string
 create an array that will store each element a string that its length is LESS THAN OR EQUAL TO column
 */
+
+function crypto (message) {
+  let str = message.replace(/[^\w\s]|_ /g, "").split(' ').join('');
+
+  return str;
+}
+
+// Test cases
+if(crypto('ZOMG! ZOMBIES!!!This1234!!./#$%^&&^%2134#$%f#f45`') === 'ZOMGZOMBIESThis12342134ff45') console.log('PASSING WITH FLYING COLORS');
+else console.log('FAILING MISERABLY, TRY AGAIN, YOU GOT DIS!')
+
+console.log(crypto('ZOMG! ZOMBIES!!!This1234!!./#$%^&&^%2134#$%f#f45`'));
